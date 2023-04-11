414fec6d440165de7356d10b8dd01d51b419c938
Tawei <c1twcny@users.noreply.github.com>
2023-04-10 21:35:23 -0400

    Update datasource.py

--- python/datasource.py
+++ python/datasource.py
@@ -1,3 +1,13 @@
+# -------------------------------------------------------------------------------------------------------
+#
+# Author: Ta-Wei
+# Date: 4/10/2023
+# Version:
+# 
+# Purpose:
+# (1) Scrap Datasource info from MITRE
+# (2) Build the (DataSource)-(Technique) edge file
+# 
 import requests
 from bs4 import BeautifulSoup
 
@@ -36,36 +46,6 @@ for i in data_sub_url_path:
 
 new_data_id_lst = list(set(data_id_lst))
 
-technique = []
-technique_m = []
-technique_id_m = []
-m = 'https://attack.mitre.org/datasources/DS0026/'
-result_m = requests.get(m)
-content_m = result_m.text
-soup_m = BeautifulSoup(content_m, 'lxml')
-table_m = soup_m.find_all('table')
-tmp_link_m = []
-for link_m in soup_m.find_all('a'):
-    tmp_link_m.append(link_m.get('href'))
-
-tmp_link_new_m = []
-for val in tmp_link_m:
-    if val != None:
-        tmp_link_new_m.append(val)
-        
-
-sub_m = 'techniques/T'
-res_m = [i for i in tmp_link_new_m if sub_m in i]
-new_res_m = list(set(res_m))
-
-for j in new_res_m:
-    technique_m.append(list(filter(None, j.split('/'))))
-    
-for e in technique_m:
-    technique_id_m.append(e[1])
-    
-new_technique_id_m = list(set(technique_id_m))
-
 techniques = []
 technique_m = []
 techique_m_id= []
@@ -100,3 +80,23 @@ for m in full_url:
     techniques.append(new_technique_m_id)
     
 data_technique = dict(zip(data_id_lst, techniques))
+
+data_full_lst = []
+technique_full_lst = []
+key_lst = list(data_technique.keys())
+
+for i in range(len(key_lst)):
+    size_n = len(data_technique[key_lst[i]])
+    tmp_lst = []
+    tmp_lst.extend(repeat(key_lst[i], size_n))
+    data_full_lst.append(tmp_lst)
+    
+    tmp_technique_lst = []
+    tmp_technique_lst = data_technique[key_lst[i]]
+    technique_full_lst.append(tmp_technique_lst)
+    
+new_data_full_lst = [item0 for sublist in data_full_lst for item0 in sublist]
+new_technique_full_lst = [item1 for sublist in technique_full_lst for item1 in sublist]
+
+tmp_df = pd.DataFrame(list(zip(new_data_full_lst, new_technique_full_lst)), columns=['data_source','technique'])
+tmp_df.to_csv("C:/Users/c1twc/OneDrive/Documents/GitHub/MITRE-ATT-CK/data_source-technique.csv", sep=',', index=False)
